46c679f8296e12b6ba548e9113b2f1ae15ca3d26
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-10-09 17:51:49 +0530

    vercel added

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
 // Initialize Passport
-app.use(passport.initialize());
+// app.use(passport.initialize());
 
 // Routes Imports
 const userRouter = require("./Routes/userRoutes");
