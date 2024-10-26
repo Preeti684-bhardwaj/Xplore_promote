c6fc66f69e4b9732256a32551e5fb892161a16ad
root <root@e2e-115-56.ssdcloudindia.net>
2024-10-26 12:51:08 +0530

    server changes

--- dbConfig/dbEnv.js
+++ dbConfig/dbEnv.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const env = {
   database: process.env.DATABASE,
-  username: "default",
+  username: "xploreuser",
   password: process.env.PASSWORD,
   host: process.env.HOST,
   dialect: process.env.DIALECT,
