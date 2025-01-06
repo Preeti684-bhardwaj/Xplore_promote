3a1e8ba19f8ef3d4db3052d52f24faaa9d411f49
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2025-01-06 11:55:40 +0530

    database issue fixed

--- dbConfig/dbEnv.js
+++ dbConfig/dbEnv.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const env = {
   database: process.env.DATABASE,
-  username: "xploreuser",
+  username: "xploreliveuser",
   password: process.env.PASSWORD,
   host: process.env.HOST,
   dialect: process.env.DIALECT,
