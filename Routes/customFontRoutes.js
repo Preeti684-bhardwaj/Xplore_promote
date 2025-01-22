b0b72680a36007c3c0f0f01cef900f92a5ea47e4
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2025-01-22 09:46:59 +0530

    encoding removed from font

--- Routes/customFontRoutes.js
+++ Routes/customFontRoutes.js
@@ -12,14 +12,14 @@ const {
 } = require("../Controller/customFontController");
 const { verifyJWt,verifySession,authorize } = require("../middleware/auth");
 const upload = require("../middleware/multer");
-const { verifyEncryption} = require('../middleware/encryption');
+// const { verifyEncryption} = require('../middleware/encryption');
 
 
 router.post("/upload/:campaignID",verifyJWt,authorize(["USER"]),verifySession,upload.array('files'),uploadCustomFont)
 router.post("/upload",verifyJWt,authorize(["USER"]),verifySession,upload.array('files'),uploadUserCustomFont)
 router.get("/getAll",verifyJWt,authorize(["USER"]),verifySession,getAllUserFonts);
 router.get("/getFontFile",downloadFontBySpecificName);
-router.get("/getFontUrl",verifyEncryption,fontUrlBySpecificName);
+router.get("/getFontUrl",fontUrlBySpecificName);
 router.get("/getAll/:campaignID",getAllFonts)
 router.get("/getOne/:id",getFontById)
 router.delete("/delete/fontWeight/:id",verifyJWt,authorize(["USER"]),verifySession,deleteFontWeight)
